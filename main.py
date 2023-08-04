c8a3fee81221a3d279a150aff6f6310003090b52
adrienallouche <adrien.allouche@gmail.com>
2023-08-04 18:50:40 +0300

    Add breakdown file, Modify main.py with getting state from user

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import turtle
 
-
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. State Game")
 
-# Adding the us map as a shape for turtle instances
+# Adding the US map as a shape for turtle instances
 us_map_image = "blank_states_img.gif"
 screen.addshape(us_map_image)
 
 # Change the screen background to the US map using turtle instance
 turtle.shape(us_map_image)
+
+data = pandas.read_csv("50_states.csv")
+print(type(data.state))
+answer_state = screen.textinput(title="Guess the State", prompt="Give another state's name")
 
 
 
